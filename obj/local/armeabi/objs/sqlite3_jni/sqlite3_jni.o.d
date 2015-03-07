f8953457076d81cd444aa0fef09fef6b55393778
Shrey Malhotra <shreks7@gmail.com>
2015-03-07 22:58:12 +0530

    Build folders
    Added build folders

---
+++ obj/local/armeabi/objs/sqlite3_jni/sqlite3_jni.o.d
@@ -0,0 +1,80 @@
+C:/Users/Shrey/Documents/GitHub/SqliteForUnity3D/Plugins/Android//obj/local/armeabi/objs/sqlite3_jni/sqlite3_jni.o: \
+ C:/Users/Shrey/Documents/GitHub/SqliteForUnity3D/Plugins/Android//jni/sqlite3_jni.c \
+ c:/NVPACK/android-ndk-r9c/platforms/android-3/arch-arm/usr/include/stdlib.h \
+ c:/NVPACK/android-ndk-r9c/platforms/android-3/arch-arm/usr/include/sys/cdefs.h \
+ c:/NVPACK/android-ndk-r9c/platforms/android-3/arch-arm/usr/include/sys/cdefs_elf.h \
+ c:/NVPACK/android-ndk-r9c/platforms/android-3/arch-arm/usr/include/android/api-level.h \
+ c:/NVPACK/android-ndk-r9c/platforms/android-3/arch-arm/usr/include/string.h \
+ c:/NVPACK/android-ndk-r9c/platforms/android-3/arch-arm/usr/include/malloc.h \
+ c:/NVPACK/android-ndk-r9c/platforms/android-3/arch-arm/usr/include/alloca.h \
+ c:/NVPACK/android-ndk-r9c/platforms/android-3/arch-arm/usr/include/strings.h \
+ c:/NVPACK/android-ndk-r9c/platforms/android-3/arch-arm/usr/include/sys/types.h \
+ c:/NVPACK/android-ndk-r9c/platforms/android-3/arch-arm/usr/include/stdint.h \
+ c:/NVPACK/android-ndk-r9c/platforms/android-3/arch-arm/usr/include/sys/_types.h \
+ c:/NVPACK/android-ndk-r9c/platforms/android-3/arch-arm/usr/include/machine/_types.h \
+ c:/NVPACK/android-ndk-r9c/platforms/android-3/arch-arm/usr/include/sys/_wchar_limits.h \
+ c:/NVPACK/android-ndk-r9c/platforms/android-3/arch-arm/usr/include/linux/posix_types.h \
+ c:/NVPACK/android-ndk-r9c/platforms/android-3/arch-arm/usr/include/linux/stddef.h \
+ c:/NVPACK/android-ndk-r9c/platforms/android-3/arch-arm/usr/include/linux/compiler.h \
+ c:/NVPACK/android-ndk-r9c/platforms/android-3/arch-arm/usr/include/asm/posix_types.h \
+ c:/NVPACK/android-ndk-r9c/platforms/android-3/arch-arm/usr/include/asm/types.h \
+ c:/NVPACK/android-ndk-r9c/platforms/android-3/arch-arm/usr/include/linux/types.h \
+ c:/NVPACK/android-ndk-r9c/platforms/android-3/arch-arm/usr/include/machine/kernel.h \
+ c:/NVPACK/android-ndk-r9c/platforms/android-3/arch-arm/usr/include/sys/sysmacros.h \
+ c:/NVPACK/android-ndk-r9c/platforms/android-3/arch-arm/usr/include/memory.h \
+ c:/NVPACK/android-ndk-r9c/platforms/android-3/arch-arm/usr/include/stdio.h \
+ C:/Users/Shrey/Documents/GitHub/SqliteForUnity3D/Plugins/Android//jni/sqlite3.h \
+ C:/Users/Shrey/Documents/GitHub/SqliteForUnity3D/Plugins/Android//jni/sqlite3_jni.h \
+ c:/NVPACK/android-ndk-r9c/platforms/android-3/arch-arm/usr/include/jni.h
+
+c:/NVPACK/android-ndk-r9c/platforms/android-3/arch-arm/usr/include/stdlib.h:
+
+c:/NVPACK/android-ndk-r9c/platforms/android-3/arch-arm/usr/include/sys/cdefs.h:
+
+c:/NVPACK/android-ndk-r9c/platforms/android-3/arch-arm/usr/include/sys/cdefs_elf.h:
+
+c:/NVPACK/android-ndk-r9c/platforms/android-3/arch-arm/usr/include/android/api-level.h:
+
+c:/NVPACK/android-ndk-r9c/platforms/android-3/arch-arm/usr/include/string.h:
+
+c:/NVPACK/android-ndk-r9c/platforms/android-3/arch-arm/usr/include/malloc.h:
+
+c:/NVPACK/android-ndk-r9c/platforms/android-3/arch-arm/usr/include/alloca.h:
+
+c:/NVPACK/android-ndk-r9c/platforms/android-3/arch-arm/usr/include/strings.h:
+
+c:/NVPACK/android-ndk-r9c/platforms/android-3/arch-arm/usr/include/sys/types.h:
+
+c:/NVPACK/android-ndk-r9c/platforms/android-3/arch-arm/usr/include/stdint.h:
+
+c:/NVPACK/android-ndk-r9c/platforms/android-3/arch-arm/usr/include/sys/_types.h:
+
+c:/NVPACK/android-ndk-r9c/platforms/android-3/arch-arm/usr/include/machine/_types.h:
+
+c:/NVPACK/android-ndk-r9c/platforms/android-3/arch-arm/usr/include/sys/_wchar_limits.h:
+
+c:/NVPACK/android-ndk-r9c/platforms/android-3/arch-arm/usr/include/linux/posix_types.h:
+
+c:/NVPACK/android-ndk-r9c/platforms/android-3/arch-arm/usr/include/linux/stddef.h:
+
+c:/NVPACK/android-ndk-r9c/platforms/android-3/arch-arm/usr/include/linux/compiler.h:
+
+c:/NVPACK/android-ndk-r9c/platforms/android-3/arch-arm/usr/include/asm/posix_types.h:
+
+c:/NVPACK/android-ndk-r9c/platforms/android-3/arch-arm/usr/include/asm/types.h:
+
+c:/NVPACK/android-ndk-r9c/platforms/android-3/arch-arm/usr/include/linux/types.h:
+
+c:/NVPACK/android-ndk-r9c/platforms/android-3/arch-arm/usr/include/machine/kernel.h:
+
+c:/NVPACK/android-ndk-r9c/platforms/android-3/arch-arm/usr/include/sys/sysmacros.h:
+
+c:/NVPACK/android-ndk-r9c/platforms/android-3/arch-arm/usr/include/memory.h:
+
+c:/NVPACK/android-ndk-r9c/platforms/android-3/arch-arm/usr/include/stdio.h:
+
+C:/Users/Shrey/Documents/GitHub/SqliteForUnity3D/Plugins/Android//jni/sqlite3.h:
+
+C:/Users/Shrey/Documents/GitHub/SqliteForUnity3D/Plugins/Android//jni/sqlite3_jni.h:
+
+c:/NVPACK/android-ndk-r9c/platforms/android-3/arch-arm/usr/include/jni.h:
